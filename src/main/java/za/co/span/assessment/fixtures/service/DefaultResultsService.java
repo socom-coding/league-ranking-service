0137e4faeee695a866ef25498b5a6f7f9330aae9
rootBotNet <wallacevanzyl@outlook.com>
2021-06-23 17:00:13 +0200

    Interface Segregation

--- src/main/java/za/co/span/assessment/fixtures/service/DefaultResultsService.java
+++ src/main/java/za/co/span/assessment/fixtures/service/DefaultResultsService.java
@@ -1,9 +1,5 @@
 package za.co.span.assessment.fixtures.service;
 
-import za.co.span.assessment.fixtures.dto.TeamDTO;
-
-import java.util.List;
-
 public interface DefaultResultsService {
     String processResult(String matchResult);
 }
